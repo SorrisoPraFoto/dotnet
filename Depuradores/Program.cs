c6310e6591a9b5ad989a33569571475a2c1cb355
marvinn2002 <marvinn2002@hotmail.com>
2021-10-17 20:44:05 -0300

    new: depuradores

---
+++ Depuradores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Depuradores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Teste");
+            Console.WriteLine(i);
+        }
+    }    
+}
