a7a2506ab13e3779bdb42a2737d77271577d4e3c
marvinn2002 <marvinn2002@hotmail.com>
2021-10-17 20:45:00 -0300

    feat: algoritmo de fibonacci errado para debug

--- Depuradores/Program.cs
+++ Depuradores/Program.cs
@@ -6,9 +6,23 @@ namespace Depuradores
     {
         static void Main(string[] args)
         {
-            int i = 0;
-            Console.WriteLine("Teste");
-            Console.WriteLine(i);
+            int result = Fibonacci(5);
+            Console.WriteLine(result);
+            Console.ReadKey(true);
         }
-    }    
+        static int Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+            return n == 0 ? n1 : n2;
+        }
+    }
 }
