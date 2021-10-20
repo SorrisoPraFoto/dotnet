908c978fb38eb7d3b1b256669d2c13c8eab79f46
marvinn2002 <marvinn2002@hotmail.com>
2021-10-19 21:25:49 -0300

    feat: funções de printar diretórios e arquivos

--- diretorios/Program.cs
+++ diretorios/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Diretorios
+{
+    class Diretorios {
+        public void printDiretorios(string raiz){
+            IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories(raiz);
+
+            foreach (var dir in listOfDirectories) {
+                Console.WriteLine(dir);
+            }
+        }
+
+        public void printArquivos(string raiz){
+            IEnumerable<string> listOfDirectories = Directory.EnumerateFiles(raiz);
+
+            foreach (var dir in listOfDirectories) {
+                Console.WriteLine(dir);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diretorios dir = new Diretorios();
+            dir.printDiretorios("stores");
+        }
+    }
+}
