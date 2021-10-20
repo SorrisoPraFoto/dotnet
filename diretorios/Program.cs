0835474b9f08a9f5b526548ddda602e101d7ca3e
marvinn2002 <marvinn2002@hotmail.com>
2021-10-19 22:25:27 -0300

    feat: cria arquivos para calcular as vendas totais de teste do json

--- diretorios/Program.cs
+++ diretorios/Program.cs
@@ -1,5 +1,10 @@
-﻿namespace Diretorios
+﻿namespace Program
 {
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
     class Diretorios {
         public void printDiretorios(string raiz){
             IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories(raiz);
@@ -24,16 +29,46 @@
                 Console.WriteLine(file);
             }
         }
+
+        public IEnumerable<string> findFiles(string raiz){
+            IEnumerable<string> listOfDirectories = Directory.EnumerateFiles(raiz);
+            return listOfDirectories;
+        }
+    }
+
+    class SalesData
+    {
+        public double Total { get; set; }
     }
 
     class Program
     {
+        static double CalculateSalesTotal(IEnumerable<string> salesFiles)
+        {
+            double salesTotal = 0;
+
+            foreach(var file in salesFiles){
+                string salesJson = File.ReadAllText(file);
+                SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson); // Converte do JSON pra string
+                salesTotal += data.Total;
+            }
+
+            return salesTotal;
+        }
         static void Main(string[] args)
         {
-            Diretorios dir = new Diretorios();
-            dir.printDiretorios("stores");
-            dir.printArquivos("stores");
-            dir.searchTextos("stores");
+            Diretorios gerenciador = new Diretorios();
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+            var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+            Directory.CreateDirectory(salesTotalDir);
+
+            var salesFiles = gerenciador.findFiles(storesDirectory);
+
+            var salesTotal = CalculateSalesTotal(salesFiles);
+
+            File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
         }
     }
 }
