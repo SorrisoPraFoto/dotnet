72e1bb85ef32002192c6ff1fb275f43db976abf9
marvinn2002 <marvinn2002@hotmail.com>
2021-10-19 21:18:07 -0300

    fix: depuradores pdb

--- Depuradores/Program.cs
+++ Depuradores/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Depuradores
 {
@@ -6,23 +7,9 @@ namespace Depuradores
     {
         static void Main(string[] args)
         {
-            int result = Fibonacci(5);
-            Console.WriteLine(result);
-            Console.ReadKey(true);
-        }
-        static int Fibonacci(int n)
-        {
-            int n1 = 0;
-            int n2 = 1;
-            int sum = 0;
-
-            for (int i = 2; i <= n; i++)
-            {
-                sum = n1 + n2;
-                n1 = n2;
-                n2 = sum;
-            }
-            return n == 0 ? n1 : n2;
+            Console.WriteLine("This message is readable by the end user.");
+            Trace.WriteLine("This is a trace message when tracing the app.");
+            Debug.WriteLine("This is a debug message just for developers.");
         }
     }
 }
